c494ce4adaf12b87f2132d3689e9fc648af447f7
Auke-Dirk <aukedirk.code@gmail.com>
2014-10-21 20:04:32 +0000

    added the two tests for constructCommandPacket

--- include/ximuapi/packet/packet_construction.h
+++ include/ximuapi/packet/packet_construction.h
@@ -23,23 +23,30 @@ namespace ximu {
 class PacketConstruction {
 public:
 // <summary>
-// Constructs an encoded command packet.
+// Constructs an encoded command packet
 // </summary>
 template<typename OutputIterator>
 static void constructCommandPacket(CommandCodes commandCode,
                                      OutputIterator dest) {
   std::vector<unsigned char> decoded = {
-(unsigned char)PacketHeaders::COMMAND,
-      (unsigned char)((ushort)commandCode >> 8),
-      (unsigned char)commandCode,
+    static_cast<unsigned char>(PacketHeaders::COMMAND),
+    static_cast<unsigned char>(static_cast<unsigned short>(commandCode) >> 8),
+    static_cast<unsigned char>(commandCode),
     0 };
 
   decoded.back() = checksum(decoded.begin(), decoded.size() - 1);
+  
+  PacketEncoding::encodePacket(decoded.begin(),decoded.end(), dest);
 }
 
 // <summary>
 // Returns the checksum, the sum all of bytes.
 // </summary>
+template<typename InputIterator>
+static unsigned char checksum(InputIterator begin, InputIterator end) {
+  return std::accumulate(begin, end, (unsigned char)0);
+}
+
 template<typename InputIterator>
 static unsigned char  checksum(InputIterator src, size_t length) {
   if (length < 0)
--- tests/packet_construction_test.cpp
+++ tests/packet_construction_test.cpp
@@ -10,13 +10,31 @@
 #include "ximuapi/enumerations.h"
 #include "ximuapi/packet/packet_construction.h"
 
-
+// Example/test on constructing commands
 int main(int argc, char* argv[]) {
-
-
+  // the factory-reset command byte sequence has been extracted
+  // from the c# gui application.
+  // The test is to create the same byte sequence
   std::vector<unsigned char> packet;
+  std::vector<unsigned char> factoryReset {0, 64, 0, 16, 144};
 
-ximu::PacketConstruction::constructCommandPacket(ximu::CommandCodes::FACTORY_RESET,back_inserter(packet));
+  ximu::PacketConstruction::constructCommandPacket(
+      ximu::CommandCodes::FACTORY_RESET, back_inserter(packet));
+
+  if (packet != factoryReset)
+    return 1;
+
+  // same test as the previous only now the command is the
+  // algorithm clear tare command.
+  packet.clear();
+  std::vector<unsigned char> algClearTare {0, 64, 1, 113, 128};
+
+  ximu::PacketConstruction::constructCommandPacket(
+      ximu::CommandCodes::ALGORITHM_CLEAR_TARE, back_inserter(packet));
+
+  if (packet != algClearTare)
+    return 1;
+
 
   return 0;
 }
